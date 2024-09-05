03d39e6a9dae611bc797eb1ac6b56531865a5039
Alexander Pshenichnyy <pshenichniy@v-mix.ru>
2024-09-05 12:27:27 +0300

    module_3_hard

---
+++ module_3_hard.py
@@ -0,0 +1,28 @@
+numeric_types = [int, float, complex]
+collections = [set, list, tuple, range]
+
+def calculate_structure_sum(*data):
+    res = 0
+    for elem in data:
+        if isinstance(elem, str):
+            res += len(elem)
+        elif type(elem) in numeric_types:
+            res += elem
+        elif isinstance(elem, bool):
+            res += int(elem)
+        elif isinstance(elem, dict):
+            res += calculate_structure_sum(*elem.items())
+        elif type(elem) in collections:
+            res += calculate_structure_sum(*elem)
+    return res
+
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
