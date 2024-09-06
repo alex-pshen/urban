4f81c67060911511a26a6fd1e9329e1f9cdd9870
Alexander Pshenichnyy <pshenichniy@v-mix.ru>
2024-09-06 11:15:17 +0300

    module_1_2 minor fixes

--- module_1_2.py
+++ module_1_2.py
@@ -3,5 +3,5 @@ time_total = 1.5
 course_name = 'Python'
 time_per_task = time_total / tasks_cnt
 
-print('Курс: ', course_name, ', всего задач:', tasks_cnt, ', затрачено часов: ',
-      + time_total, ', среднее время выполнения ', + time_per_task, ' часа.', sep='' )
+print('Курс: ', course_name, ', всего задач: ', tasks_cnt, ', затрачено часов: ',
+      time_total, ', среднее время выполнения ', time_per_task, ' часа.', sep='' )
