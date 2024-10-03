d6da53a95984e8ddfff111aeaf133933117b50b1
Alexander Pshenichnyy <pshenichniy@v-mix.ru>
2024-10-03 10:54:32 +0300

    module_6_2: remove comments

--- module_6_2.py
+++ module_6_2.py
@@ -33,16 +33,12 @@ class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
 
 
-# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 vehicle1 = Sedan("Fedos", "Toyota Mark II", "blue", 500)
 
-# Изначальные свойства
 vehicle1.print_info()
 
-# Меняем свойства (в т.ч. вызывая методы)
 vehicle1.set_color("Pink")
 vehicle1.set_color("BLACK")
 vehicle1.owner = "Vasyok"
 
-# Проверяем что поменялось
 vehicle1.print_info()
