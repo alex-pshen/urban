eb5530a0c1b5d0d6d8b1ad19785a070877a7848e
Alexander Pshenichnyy <pshenichniy@v-mix.ru>
2024-11-01 19:25:13 +0300

    module_7_2

---
+++ module_7_2.py
@@ -0,0 +1,21 @@
+def custom_write(file_name, strings):
+    strings_positions = {}
+    with open(file_name, "w", encoding="utf-8") as file:
+        for i in range(len(strings)):
+            s = strings[i]
+            strings_positions[(i + 1, file.tell())] = s
+            file.write(s + "\n")
+
+    return strings_positions
+
+
+info = [
+    "Text for tell.",
+    "Используйте кодировку utf-8.",
+    "Because there are 2 languages!",
+    "Спасибо!",
+]
+
+result = custom_write("test.txt", info)
+for elem in result.items():
+    print(elem)
