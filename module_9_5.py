aaf5ad86eddbb08d9e6ab46634d1b58eed78d24d
Alexander Pshenichnyy <pshenichniy@v-mix.ru>
2024-11-05 15:21:36 +0300

    module_9_5

---
+++ module_9_5.py
@@ -0,0 +1,53 @@
+class StepValueError(ValueError):
+    pass
+
+
+sign = lambda x: 0 if abs(x) == 0 else x / abs(x)
+
+
+class Iterator:
+    def __init__(self, start, stop, step=1) -> None:
+        if step == 0:
+            raise StepValueError("Шаг не может быть равен 0")
+
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.pointer = self.start
+
+    def __iter__(self):
+        self.pointer = self.start
+        return self
+
+    def __next__(self):
+        if sign(self.step) != sign(self.stop - self.pointer):
+            raise StopIteration
+        res = self.pointer
+        self.pointer += self.step
+        return res
+
+
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=" ")
+except StepValueError:
+    print("Шаг указан неверно")
+
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
+
+for i in iter2:
+    print(i, end=" ")
+print()
+for i in iter3:
+    print(i, end=" ")
+print()
+for i in iter4:
+    print(i, end=" ")
+print()
+for i in iter5:
+    print(i, end=" ")
+print()
