ab36a12ffd494a62a01fcffbd9fe3ea095449145
Alexander Pshenichnyy <pshenichniy@v-mix.ru>
2024-11-06 10:54:15 +0300

    module_9_7: added decorator on decorator

--- module_9_7.py
+++ module_9_7.py
@@ -1,31 +1,40 @@
+def check_prime(n):
+    if n < 2:
+        raise ValueError("Для чисел меньше 2, простота не определена")
+    for d in range(2, n // 2 + 1):
+        if not n % d:
+            return False
+    return True
+
+
 def is_prime(func):
-    def wrapper(a, b, c):
-        n = func(a, b, c)
-        if n < 2:
-            raise ValueError()
-        res = True
-        for d in range(2, n // 2 + 1):
-            if not n % d:
-                res = False
-                break
-        print("Простое" if res else "Составное")
+    def wrapper(*args, **kwargs):
+        n = func(*args, **kwargs)
+        if check_prime(n):
+            print("Простое")
+        else:
+            print("Составное")
         return n
 
     return wrapper
 
 
+def print_result(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print(result)
+
+    return wrapper
+
+
+@print_result
 @is_prime
 def sum_three(a, b, c):
     return a + b + c
 
 
-result = sum_three(2, 3, 6)
-print(result)
-result = sum_three(3, 3, 6)
-print(result)
-result = sum_three(1, 1, 2)
-print(result)
-result = sum_three(1, 1, 0)
-print(result)
-result = sum_three(1, 0, 0)
-print(result)
+sum_three(2, 3, 6)
+sum_three(3, 3, 6)
+sum_three(1, 1, 2)
+sum_three(1, 1, 0)
+sum_three(1, 0, 0)
