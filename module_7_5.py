b305ab3a01dd16f91e7f977710993c5a9d80e50d
Alexander Pshenichnyy <pshenichniy@v-mix.ru>
2024-12-02 13:15:05 +0300

    module_7_5: fixed filepath in getsize

--- module_7_5.py
+++ module_7_5.py
@@ -5,10 +5,12 @@ import time
 directory = "."
 for root, dirs, files in os.walk(directory):
     for file in files:
+        print(root, file)
         filepath = os.path.join(root, file)
+        print(filepath)
         filetime = os.path.getmtime(filepath)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.path.getsize(file)
+        filesize = os.path.getsize(filepath)
         parent_dir = os.path.dirname(filepath)
         print(
             f"Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}"
